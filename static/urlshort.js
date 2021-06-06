9e7c25d6251f498eb31a7a564eec9e9e3cd9fcce
Gopal Choudhary <raunakchd1@gmail.com>
2021-06-06 12:38:58 +0530

    first commit

---
+++ static/urlshort.js
@@ -0,0 +1,38 @@
+window.onload=()=>{
+    const inputVal=document.querySelector("input");
+    const button=document.querySelector(".btn");
+    const copyBtn=document.querySelector(".copy-btn");
+    
+    const form=document.querySelector(".form");
+    form.addEventListener("submit",(event)=>{
+        event.preventDefault();
+        const formData = encodeURIComponent(inputVal.name)+'='+encodeURIComponent(inputVal.value);
+
+        const options = {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }),
+            body : formData
+        }
+
+        fetch('http://localhost:3000/',options)
+        .then(response=>response.json())
+        .then(data=>{
+            inputVal.value=data.shortUrl;
+            button.disabled=true;
+            copyBtn.addEventListener("click",()=>{
+                copyBtn.textContent="Copied";
+                inputVal.select();
+                document.execCommand('copy');
+
+            });
+            inputVal.addEventListener("input",()=>{
+                button.disabled=false;
+                copyBtn.textContent="Copy URL";
+            });
+        });
+    });
+    
+
+}
